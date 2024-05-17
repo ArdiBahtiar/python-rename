9eca13d62d58c1893009cfc23dda1619741cc38b
ArdiBahtiar <muhammadardibachtiar@gmail.com>
2024-05-17 11:03:12 +0700

    done production

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+def main():
+    # i = 1
+    mulai_nomor = int(input("Mulai dari nomor berapa?"))    # ini buat kalo mulai lagi dari 201, 401, dst
+    nama_file = input("Nama filenya nanti apa?")            # ini bebas, pokoknya ngikut buat semua
+
+    path = "D:\\tung\\python\\rename\\gambar"
+    for filename in os.listdir(path):
+        my_dest = nama_file + str(mulai_nomor) + ".png"
+        my_source = path + "/" + filename
+        my_dest = path + "/" + my_dest
+        os.rename(my_source, my_dest)
+        mulai_nomor += 1
+    
+# print("Gambar sudah diurutkan")    
+if __name__ == '__main__':
+    main()
